de21ee2bc449fd2cbc399fba88ce043b59da1a7f
ST10407732 <131349864+ST10407732@users.noreply.github.com>
2024-09-30 20:25:49 +0200

    Add files via upload

---
+++ AdminWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prog2BIceTask1
+{
+    /// <summary>
+    /// Interaction logic for AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Transactions.ItemsSource = TransactionUtil.transactions;
+            Products.ItemsSource = ProductUtil.products;
+            Products.Columns[0].Visibility = Visibility.Hidden;
+            Products.Columns[4].Visibility = Visibility.Hidden;
+
+        }
+    }
+}
